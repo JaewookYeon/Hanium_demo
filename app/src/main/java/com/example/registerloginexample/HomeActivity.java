ab87a1c8a43f099b441624101e5b0c9ef33c865b
LYUHAERIM <haerim0228@skuniv.ac.kr>
2023-07-14 14:38:44 +0900

    메뉴바

--- app/src/main/java/com/example/registerloginexample/HomeActivity.java
+++ app/src/main/java/com/example/registerloginexample/HomeActivity.java
@@ -1,17 +1,60 @@
 package com.example.registerloginexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView; //바텀 네이게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag1 frag1;
+    private Frag2 frag2;
+    private Frag3 frag3;
+    private Frag4 frag4;
+    private Frag5 frag5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_locker:
+                        setFrag(0);
+                        break;
+                    case R.id.action_refrigerator:
+                        setFrag(1);
+                        break;
+                    case R.id.action_home:
+                        setFrag(2);
+                        break;
+                    case R.id.action_board:
+                        setFrag(3);
+                        break;
+                    case R.id.action_myPage:
+                        setFrag(4);
+                        break;
+                }
+                return true;
+            }
+        });
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        frag4 = new Frag4();
+        frag5 = new Frag5();
+        setFrag(2); //첫 프래그먼트화면 홈화면(3)
 
         // HomeFragment 생성
         HomeFragment homeFragment = new HomeFragment();
@@ -21,5 +64,34 @@ public class HomeActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.layout, homeFragment);
         fragmentTransaction.commit();
+    }
+    
+    //프래그먼트 교체가 일어나는 실행문
+    private void setFrag(int n) {
+        fm =getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, frag1);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, frag2);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, frag3);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, frag4);
+                ft.commit();
+                break;
+            case 4:
+                ft.replace(R.id.main_frame, frag5);
+                ft.commit();
+                break;
+
+        }
     }
 }
