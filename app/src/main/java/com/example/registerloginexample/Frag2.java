6e01261fd8945ae2f90e2c69eb5c7b48b8d6c849
LYUHAERIM <haerim0228@skuniv.ac.kr>
2023-07-14 17:05:49 +0900

    메뉴바 합치기 1

--- app/src/main/java/com/example/registerloginexample/Frag2.java
+++ app/src/main/java/com/example/registerloginexample/Frag2.java
@@ -1,9 +1,11 @@
 package com.example.registerloginexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +14,21 @@ import androidx.fragment.app.Fragment;
 public class Frag2 extends Fragment {
 
     private View view;
+    private ImageView btn_want;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag2, container, false);
 
+        btn_want = view.findViewById(R.id.btn_want);
+        btn_want.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Want.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/com/example/registerloginexample/MainActivity.java
+++ app/src/main/java/com/example/registerloginexample/MainActivity.java
@@ -1,40 +1,88 @@
 package com.example.registerloginexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_id, tv_pass;
-    private Button btn_home;
-
+    private BottomNavigationView bottomNavigationView; //바텀 네이게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag1 frag1;
+    private Frag2 frag2;
+    private Frag3 frag3;
+    private Frag4 frag4;
+    private Frag5 frag5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_id = findViewById(R.id.tv_id);
-        tv_pass = findViewById(R.id.tv_pass);
-        btn_home = findViewById(R.id.btn_home);
-
-        Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-        String userPass = intent.getStringExtra("userPass");
-
-        tv_id.setText(userID);
-        tv_pass.setText(userPass);
-
-        btn_home.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                // HomeActivity 시작
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_locker:
+                        setFrag(0);
+                        break;
+                    case R.id.action_refrigerator:
+                        setFrag(1);
+                        break;
+                    case R.id.action_home:
+                        setFrag(2);
+                        break;
+                    case R.id.action_board:
+                        setFrag(3);
+                        break;
+                    case R.id.action_myPage:
+                        setFrag(4);
+                        break;
+                }
+                return true;
             }
         });
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        frag4 = new Frag4();
+        frag5 = new Frag5();
+        setFrag(2); //첫 프래그먼트화면 홈화면(3)
+    }
+
+    //프래그먼트 교체가 일어나는 실행문
+    private void setFrag(int n) {
+        fm =getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, frag1);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, frag2);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, frag3);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, frag4);
+                ft.commit();
+                break;
+            case 4:
+                ft.replace(R.id.main_frame, frag5);
+                ft.commit();
+                break;
+
+        }
     }
 }
