25842e7c5ad2913a2dc6acd3320747faae67165c
LYUHAERIM <haerim0228@skuniv.ac.kr>
2023-08-12 21:24:45 +0900

    보관합, 회원가입 수정

--- app/src/main/java/com/example/registerloginexample/RegisterActivity.java
+++ app/src/main/java/com/example/registerloginexample/RegisterActivity.java
@@ -1,76 +1,83 @@
 package com.example.registerloginexample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText et_id,et_pass,et_name,et_phone,et_address;
-    private Button btn_register;
+    private EditText et_id, et_pass, et_pass2, et_name, et_nickname, et_phone, et_address;
+    private Button btn_register, btn_doubleCheck;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { //액티비티 시작시 처음으로 실행되는 생명주기
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //아이디 값 찾아주기
-        et_id=findViewById(R.id.et_id);
-        et_pass=findViewById(R.id.et_pass);
-        et_name=findViewById(R.id.et_name);
-        et_phone=findViewById(R.id.et_phone);
-        et_address=findViewById(R.id.et_address);
+        // 아이디 값 찾아주기
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_pass);
+        et_pass2 = findViewById(R.id.et_pass2);
+        et_name = findViewById(R.id.et_name);
+        et_nickname = findViewById(R.id.et_nickname);
+        et_phone = findViewById(R.id.et_phone);
+        et_address = findViewById(R.id.et_address);
 
-        //회원가입 버튼 클릭 시 수행
-        btn_register=findViewById(R.id.btn_register);
+        // 회원가입 버튼 클릭 시 수행
+        btn_register = findViewById(R.id.btn_register);
+        btn_doubleCheck = findViewById(R.id.btn_doubleCheck);
+
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //EditText에 현재 입력되어 있는 값을 얻어온다.
-                final String login_id=et_id.getText().toString();
-                final String login_password=et_pass.getText().toString();
-                final String name=et_name.getText().toString();
-                final String phone=et_phone.getText().toString();
-                final String address=et_address.getText().toString();
+                final String login_id = et_id.getText().toString();
+                final String login_password = et_pass.getText().toString();
+                final String name = et_name.getText().toString();
+                final String nickname = et_nickname.getText().toString();
+                final String phone = et_phone.getText().toString();
+                final String address = et_address.getText().toString();
 
-                Response.Listener<String> responseListener=new Response.Listener<String>() {
+                // 비밀번호 일치 여부 확인
+                if (!login_password.equals(et_pass2.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
-
                     public void onResponse(String response) {
                         try {
-                            JSONObject jsonObject=new JSONObject(response);
-                            boolean success=jsonObject.getBoolean("success");
-                            if(success){ //회원등록에 성공한 경우
-                                Toast.makeText(getApplicationContext(),"회원 등록에 성공하였습니다.",Toast.LENGTH_SHORT).show();
-                                Intent intent=new Intent(RegisterActivity.this, LoginActivity.class);
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                Toast.makeText(getApplicationContext(), "회원등록에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                                 startActivity(intent);
-                            }else{//회원등록에 실패한 경우
-                                Toast.makeText(getApplicationContext(),"회원 등록에 실패하였습니다.",Toast.LENGTH_SHORT).show();
-
-                                return;
+                            } else {
+                                Toast.makeText(getApplicationContext(), "회원등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                    }public void onErrorResponse(VolleyError error) {
+                    }
+
+                    public void onErrorResponse(VolleyError error) {
                         Toast.makeText(getApplicationContext(), "회원가입 실패: " + error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 };
-                //서버로 Volley를 이용해서 요청
-                RegisterRequest registerRequest=new RegisterRequest(login_id,login_password,name,phone,address,responseListener);
-                RequestQueue queue= Volley.newRequestQueue(RegisterActivity.this);
+
+                // 서버로 Volley를 이용해서 요청
+                RegisterRequest registerRequest = new RegisterRequest(login_id, login_password, name, phone, address, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
                 queue.add(registerRequest);
             }
         });
--- app/src/main/java/com/example/registerloginexample/lockerActivity.java
+++ app/src/main/java/com/example/registerloginexample/lockerActivity.java
@@ -12,12 +12,14 @@ import android.widget.Toast;
 public class lockerActivity extends AppCompatActivity {
 
     private EditText editText;
+    private StringBuilder inputStringBuilder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locker);
 
-        Button btn [] = new Button[12];
+        Button btn [] = new Button[13]; // Change the array size to 13
 
         btn[0] = findViewById(R.id.btn0);
         btn[1] = findViewById(R.id.btn1);
@@ -29,31 +31,43 @@ public class lockerActivity extends AppCompatActivity {
         btn[7] = findViewById(R.id.btn7);
         btn[8] = findViewById(R.id.btn8);
         btn[9] = findViewById(R.id.btn9);
-        btn[10] = findViewById(R.id.btn_ok);
-        btn[11] = findViewById(R.id.btn_clear);
+        btn[10] = findViewById(R.id.btn_C); // Use btn_C for clear
+        btn[11] = findViewById(R.id.btn_ok);
+        btn[12] = findViewById(R.id.btn_AC); // Use btn_AC for AC
         editText = findViewById(R.id.displayNumber);
 
-        for (int i=0; i<12;i++) {
-            btn[i].setOnClickListener(new View.OnClickListener() {
+        inputStringBuilder = new StringBuilder();
 
+        for (int i=0; i<13; i++) { // Change the loop to run through all 13 buttons
+            btn[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    editText.clearFocus();
                     Button btn = (Button) v;
-                    editText.append(btn.getText().toString());
-                    if (editText.getText().toString().equals("0")) {
+                    String buttonText = btn.getText().toString();
+
+                    if (buttonText.equals("AC")) {
+                        inputStringBuilder.setLength(0);
                         editText.setText("");
-                    }
-                    if (btn.getText().toString().equals("clear")) {
-                        editText.setText("");
-                    }
-                    if (btn.getText().toString().equals("Ok")) {
-                        //String str = editText.getText().toString();
-                        //editText.setText(str.substring(0, str.length() - 2));
-                        //Toast.makeText(getApplicationContext(), editText.getText(), Toast.LENGTH_SHORT).show();
-                        Toast.makeText(getApplicationContext(), "보관함이 열렸습니다.", Toast.LENGTH_SHORT).show();
-                        editText.setText("");
-                        Intent intent = new Intent(getApplicationContext(), Frag3.class);
-                        startActivity(intent);
+                    } else if (buttonText.equals("Ok")) {
+                        if (inputStringBuilder.length() == 4) {
+                            Toast.makeText(getApplicationContext(), "보관함이 열렸습니다.", Toast.LENGTH_SHORT).show();
+                            editText.setText("");
+                            Intent intent = new Intent(getApplicationContext(), Frag3.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "4자리 숫자를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        }
+                    } else if (buttonText.equals("C")) { // Check for "C"
+                        if (inputStringBuilder.length() > 0) {
+                            inputStringBuilder.deleteCharAt(inputStringBuilder.length() - 1);
+                            editText.setText(inputStringBuilder.toString());
+                        }
+                    } else {
+                        if (inputStringBuilder.length() < 4) {
+                            inputStringBuilder.append(buttonText);
+                            editText.setText(inputStringBuilder.toString());
+                        }
                     }
                 }
             });
