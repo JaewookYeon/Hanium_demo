505b456070227d438c5367d3f075897c2bcab6d9
LYUHAERIM <haerim0228@skuniv.ac.kr>
2023-09-13 19:32:37 +0900

    레이아웃 수정 및 툴바 적용

--- app/src/main/java/com/example/registerloginexample/Frag3.java
+++ app/src/main/java/com/example/registerloginexample/Frag3.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toolbar;
 import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
