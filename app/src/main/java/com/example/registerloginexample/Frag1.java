ecf7eb0a2324ead40cf3be8f0054021c72ee454a
LYUHAERIM <haerim0228@skuniv.ac.kr>
2023-09-14 04:03:40 +0900

    frag1 update

--- app/src/main/java/com/example/registerloginexample/Frag1.java
+++ app/src/main/java/com/example/registerloginexample/Frag1.java
@@ -6,26 +6,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class Frag1 extends Fragment {
 
     private View view;
-    private Button lockerButton;
+    private ImageButton lockerButton;
+    private FloatingActionButton DescripButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag1, container, false);
         lockerButton = view.findViewById(R.id.lockerButton);
+        DescripButton = view.findViewById(R.id.DescripButton);
 
         lockerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), lockerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        DescripButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), lockerDesActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/registerloginexample/lockerDesActivity.java
@@ -0,0 +1,14 @@
+package com.example.registerloginexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lockerDesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locker_des);
+    }
+}
