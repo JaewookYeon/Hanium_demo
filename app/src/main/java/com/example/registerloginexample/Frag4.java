4ec79ad28cb498f1ac8a56c86b2480d27cf857f5
kyeongaYU <ruddk658@naver.com>
2023-09-16 01:13:41 +0900

    상품 db에서 띄우는 거 하는 중

--- app/src/main/java/com/example/registerloginexample/Frag4.java
+++ app/src/main/java/com/example/registerloginexample/Frag4.java
@@ -15,6 +15,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,19 +33,71 @@ public class Frag4 extends Fragment implements View.OnClickListener {
     private String login_id; // Frag4 내에서 사용하는 변수
     // 데이터베이스에서 게시물 데이터를 가져오는 메서드
     private void loadBoardDataFromDatabase() {
-        // 데이터베이스에서 게시물 데이터를 가져오는 코드를 작성합니다.
-        // 이 부분은 데이터베이스 연동 방법에 따라 다를 수 있습니다.
+        Log.d("MyApp", "Attempting to load data from the database."); // 로그 추가
+        // 데이터베이스에서 게시물 데이터를 가져옵니다.
+        List<Board> boardData = getBoardDataFromDatabase();
 
-        // 예시: 가져온 데이터를 mBoardList에 추가합니다.
-        //List<Board> boardData = getBoardDataFromDatabase(); // 데이터베이스에서 데이터 가져오기
-        //mBoardList.addAll(boardData); // 가져온 데이터를 mBoardList에 추가
-        //mAdapter.notifyDataSetChanged(); // 어댑터에 데이터 변경 알림
+        // 가져온 데이터를 mBoardList에 추가합니다.
+        mBoardList.clear(); // 기존 데이터를 모두 제거합니다.
+        mBoardList.addAll(boardData); // 새로운 데이터를 mBoardList에 추가합니다.
+        mAdapter.notifyDataSetChanged(); // 어댑터에 데이터 변경 알림
     }
 
-    //private List<Board> getBoardDataFromDatabase() {
-    //}
 
-    // loadBoardDataFromDatabase() 메서드를 onCreateView() 내에서 호출합니다.
+    private List<Board> getBoardDataFromDatabase() {
+        Connection connection = null;
+        List<Board> boardList = new ArrayList<>();
+        try {
+            // 데이터베이스 연결 설정
+            String url = "jdbc:mysql://localhost:3307/hanium_api"; // 데이터베이스 URL
+            String username = "hanium";
+            String password = "1234";
+
+            // JDBC 드라이버 로드
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 데이터베이스 연결
+            connection = DriverManager.getConnection(url, username, password);
+
+            // SQL 쿼리 실행
+            String sql = "SELECT * FROM BOARD";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            // 결과를 Board 객체로 변환하여 리스트에 추가
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String title = resultSet.getString("b_title");
+                String content = resultSet.getString("b_content");
+                String name = resultSet.getString("custid");
+
+                Board board = new Board(id, title, content, name);
+                boardList.add(board);
+            }
+
+            // 리소스 해제
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Log.e("MyApp", "SQL Exception1: " + e.getMessage()); // SQL 예외 메시지 출력
+            return null;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            Log.e("MyApp", "Class Not Found Exception: " + e.getMessage()); // 클래스를 찾을 수 없는 예외 메시지 출력
+        } finally {
+            // 데이터베이스 연결 닫기
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return boardList;
+    }
+
 
     @Nullable
     @Override
@@ -53,11 +110,9 @@ public class Frag4 extends Fragment implements View.OnClickListener {
         mMainWriteButton.setOnClickListener(this);
 
         mBoardList = new ArrayList<>();
-        mBoardList.add(new Board(null,"반갑습니다 여러분", null,"android"));
-        mBoardList.add(new Board(null,"안녕", null,"Server"));
-        mBoardList.add(new Board(null,"Hello", null,"Java"));
-        mBoardList.add(new Board(null,"Hi", null,"php"));
-        mBoardList.add(new Board(null,"ㅋㅋ", null,"Python"));
+
+        // 데이터베이스에서 게시물 데이터를 가져옵니다.
+        loadBoardDataFromDatabase();
 
         mAdapter = new MainAdapter(mBoardList);
         mMainRecyclerView.setAdapter(mAdapter);
@@ -65,11 +120,38 @@ public class Frag4 extends Fragment implements View.OnClickListener {
         // login_id를 받아오는 코드 (Frag4 내에서 필요한 경우)
         Bundle bundle4 = getArguments();
         if (bundle4 != null) {
+            Connection connection = null;
+            try{
+                String url = "jdbc:mysql://localhost:3306/hanium_api"; // 데이터베이스 URL
+                String username = "hanium";
+                String password = "1234";
+
+                // JDBC 드라이버 로드
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                // 데이터베이스 연결
+                connection = DriverManager.getConnection(url, username, password);
+            }catch(SQLException e){
+                Log.e("MyApp", "SQL Exception2: " + e.getMessage());
+                return null;
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
             login_id = bundle4.getString("login_id");
             Log.d("Frag4", "Received login_id: " + login_id);
+
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
         return view;
     }
+
 
     @Override
     public void onClick(View v) {
